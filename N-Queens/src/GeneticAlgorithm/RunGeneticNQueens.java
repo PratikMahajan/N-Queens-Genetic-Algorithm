5c928862ae991b16a442f548c71445eb839eb44e
PratikMahajan <Pratik@mahajan.xyz>
2018-12-04 18:31:00 -0500

    check commit

--- N-Queens/src/GeneticAlgorithm/RunGeneticNQueens.java
+++ N-Queens/src/GeneticAlgorithm/RunGeneticNQueens.java
@@ -5,10 +5,190 @@
  */
 package GeneticAlgorithm;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author mahajan
  */
 public class RunGeneticNQueens {
+    
+    Writer logWriter;
+    GeneticAlgorithm ga;
+    int MAX_RUN;
+    int MAX_LENGTH;
+    long[] runtimes;
+
+
+    public RunGeneticNQueens() {
+            logWriter = new Writer();
+            MAX_RUN = 50;
+            runtimes = new long[MAX_RUN];
+    }
+
+
+    public void test(int maxLength, double mutationRate, int maxEpoch) {
+            MAX_LENGTH = maxLength;
+            ga = new GeneticAlgorithm(MAX_LENGTH, maxEpoch, mutationRate);
+            long testStart = System.nanoTime();
+            String filepath = "GA-N"+MAX_LENGTH+"-"+mutationRate+"-"+maxEpoch+".txt";
+            long startTime = 0;
+    long endTime = 0;
+    long totalTime = 0;
+    int fail = 0;
+    int success = 0;
+
+            logParameters();
+
+    for(int i = 0; i < MAX_RUN; ) {												//run 50 sucess to pass passing criteria
+            startTime = System.nanoTime();
+            if(ga.runGA()) {
+                    endTime = System.nanoTime();
+                    totalTime = endTime - startTime;
+
+                    System.out.println("Done");
+                    System.out.println("run "+(i+1));
+            System.out.println("time in nanoseconds: "+totalTime);
+            System.out.println("Success!");
+
+            runtimes[i] = totalTime;
+            i++;
+            success++;
+
+
+            logWriter.add((String)("Run: "+i));
+            logWriter.add((String)("Runtime in nanoseconds: "+totalTime));
+            logWriter.add((String)("Found at epoch: "+ga.getEpoch()));
+            logWriter.add((String)("Population size: "+ga.getPopulationSize()));
+            logWriter.add("");
+
+            for(Chromosome c: ga.getSolutions()) {								//write solutions to log file
+                                    logWriter.add(c);
+                                    logWriter.add("");
+                    }
+            } else {																//count failures for failing criteria
+                    fail++;
+                    System.out.println("Fail!");
+            }
+
+            if(fail >= 100) {
+                    System.out.println("Change Parametres");
+                    break;
+            }
+            startTime = 0;															//reset time
+            endTime = 0;
+            totalTime = 0;
+    }
+
+    System.out.println("Success: " +success);
+    System.out.println("failures: "+fail);
+    logWriter.add("Runtime summary");
+    logWriter.add("");
+
+            for(int x = 0; x < runtimes.length; x++){									//print runtime summary
+                    logWriter.add(Long.toString(runtimes[x]));
+            }
+
+            long testEnd = System.nanoTime();
+            logWriter.add(Long.toString(testStart));
+            logWriter.add(Long.toString(testEnd));
+            logWriter.add(Long.toString(testEnd - testStart));
+
+
+    logWriter.writeFile(filepath);
+    printRuntimes();
+    }
+
+
+    public void logParameters() {
+    logWriter.add("Genetic Algorithm");
+    logWriter.add("Parameters");
+    logWriter.add((String)("MAX_LENGTH/N: "+MAX_LENGTH));
+    logWriter.add((String)("STARTING_POPULATION: "+ga.getSTART_SIZE()));
+    logWriter.add((String)("MAX_EPOCHS: "+ga.getMAX_EPOCHS()));
+    logWriter.add((String)("MATING_PROBABILITY: "+ga.getMATING_PROBABILITY()));
+    logWriter.add((String)("MUTATION_RATE: "+ga.getMUTATION_RATE()));
+    logWriter.add((String)("MIN_SELECTED_PARENTS: "+ga.getMIN_SELECT()));
+    logWriter.add((String)("MAX_SELECTED_PARENTS: "+ga.getMAX_SELECT()));
+    logWriter.add((String)("OFFSPRING_PER_GENERATION: "+ga.getOFFSPRING_PER_GENERATION()));
+    logWriter.add((String)("MINIMUM_SHUFFLES: "+ga.getMINIMUM_SHUFFLES()));
+    logWriter.add((String)("MAXIMUM_SHUFFLES: "+ga.getMAXIMUM_SHUFFLES()));
+    logWriter.add("");
+    }
+
+
+    
+    public void printRuntimes() {
+            for(long x: runtimes){
+                    System.out.println("run with time "+x+" nanoseconds");
+            }	
+    }
+
+    public static void main(String args[]) {
+            RunGeneticNQueens tester = new RunGeneticNQueens();
+
+            tester.test(8, 0.001, 1000);
+/*		tester.test(8, 0.001, 1000);
+            tester.test(12, 0.001, 1000);
+            tester.test(16, 0.001, 1000);
+            tester.test(20, 0.001, 1000);
+
+            tester.test(20, 0.001, 1000);
+            tester.test(20, 0.005, 1000);
+            tester.test(20, 0.01, 1000);
+            tester.test(20, 0.05, 1000);
+            tester.test(20, 0.1, 1000);
+
+            tester.test(16, 0.001, 5000);
+            tester.test(16, 0.005, 5000);
+            tester.test(16, 0.01, 5000);
+            tester.test(16, 0.05, 5000);
+            tester.test(16, 0.1, 5000);
+
+            tester.test(20, 0.001, 5000);
+            tester.test(20, 0.005, 5000);
+            tester.test(20, 0.01, 5000);
+            tester.test(20, 0.05, 5000);
+            tester.test(20, 0.1, 5000);
+
+            tester.test(16, 0.001, 1000);
+            tester.test(16, 0.005, 1000);
+            tester.test(16, 0.01, 1000);
+            tester.test(16, 0.05, 1000);
+            tester.test(16, 0.1, 1000);
+
+            tester.test(16, 0.001, 5000);
+            tester.test(16, 0.005, 5000);
+            tester.test(16, 0.01, 5000);
+            tester.test(16, 0.05, 5000);
+            tester.test(16, 0.1, 5000);
+            tester.test(16, 0.001, 10000);
+            tester.test(16, 0.005, 10000);
+            tester.test(16, 0.01, 10000);
+            tester.test(16, 0.05, 10000);
+            tester.test(16, 0.1, 10000);
+            tester.test(20, 0.001, 1000);
+            tester.test(20, 0.005, 1000);
+            tester.test(20, 0.01, 1000);
+            tester.test(20, 0.05, 1000);
+            tester.test(20, 0.1, 1000);
+
+            tester.test(20, 0.001, 5000);
+            tester.test(20, 0.005, 5000);
+            tester.test(20, 0.01, 5000);
+            tester.test(20, 0.05, 5000);
+            tester.test(20, 0.1, 5000);
+            tester.test(20, 0.001, 10000);
+            tester.test(20, 0.005, 10000);
+            tester.test(20, 0.01, 10000);
+            tester.test(20, 0.05, 10000);
+            tester.test(20, 0.1, 10000);	
+*/
+
+    }
+
+    
     
 }
